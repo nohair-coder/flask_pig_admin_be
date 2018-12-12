fb740cdaaf67b28a58358c2cd7f8e414d7a5ef07
lxfriday <liu3248184446@outlook.com>
2018-12-12 19:34:13 +0800

    [modify] 对内层函数添加 functools.wrap 修饰器

--- app/common/util/new_thread.py
+++ app/common/util/new_thread.py
@@ -1,7 +1,9 @@
 # coding: utf8
 from threading import Thread
+import functools
 
 def asyncFunc(f):
+    @functools.wraps(f)
     def wrapper(*args, **kwargs):
         thr = Thread(target = f, args = args, kwargs = kwargs)
         thr.start()
