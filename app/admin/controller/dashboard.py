05425d8fcf8099772aa5cee5119b46260eb3097e
lxfriday <liu3248184446@outlook.com>
2018-12-23 14:32:40 +0800

    [modify] dashboard 依照测定站状态排序

--- app/admin/controller/dashboard.py
+++ app/admin/controller/dashboard.py
@@ -11,17 +11,36 @@ from app.common.errorcode import error_code
 def stationinfo():
     try:
         res = StationInfo.query.all()
-        ret = []
+        station_off = [] # off 状态的排在前面
+        station_err = [] # err 状态排在中间
+        station_on = [] # on 正常状态的排在第三类
         for r in res:
-            ret.append(
-                {
+            if r.status == 'off':
+                station_off.append({
                     'id': r.id, # 记录的 id
                     'stationid': r.stationid.lstrip('0'), # 测定站 id
                     'status': r.status, # 测定站状态
                     'changetime': r.changetime, # 测定站状态修改时间
                     'errorcode': r.errorcode, # 错误码
-                }
-            )
+                })
+            elif r.status == 'on' and r.errorcode != '00000':
+                station_err.append({
+                    'id': r.id, # 记录的 id
+                    'stationid': r.stationid.lstrip('0'), # 测定站 id
+                    'status': r.status, # 测定站状态
+                    'changetime': r.changetime, # 测定站状态修改时间
+                    'errorcode': r.errorcode, # 错误码
+                })
+            else:
+                station_on.append({
+                    'id': r.id, # 记录的 id
+                    'stationid': r.stationid.lstrip('0'), # 测定站 id
+                    'status': r.status, # 测定站状态
+                    'changetime': r.changetime, # 测定站状态修改时间
+                    'errorcode': r.errorcode, # 错误码
+                })
+
+        ret = station_off + station_err + station_on
     except Exception as e:
         error_logger(e)
         error_logger(error_code['1000_5001'])
